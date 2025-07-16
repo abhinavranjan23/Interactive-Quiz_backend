abfb30651fc9974c44bdcbfa7e56ef719ad91d49
Abhinav Ranjan <abhinavranjan1523@gmail.com>
2025-07-16 17:43:28 +0530

    Final commit

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://brainrush.abhinavranjan.me/"],
+    origin: ["http://localhost:5173", "https://brainrush.abhinavranjan.me"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
